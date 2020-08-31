579edbd372c969bf9af63c5d20d503640834cf6c
Anders Børjesson <anbo@zealand.dk>
2020-08-31 09:27:13 +0200

    Add project files.

---
+++ TeacherLib/Teacher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TeacherLib
+{
+    public class Teacher
+    {
+        private int _salary;
+        private string _name;
+        public static readonly double Pi = Math.PI;
+
+        public Teacher(string name, int salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 2) throw new ArgumentException();
+                _name = value;
+            }
+        }
+
+        public int Salary
+        {
+            get => _salary;
+            set
+            {
+                //if (value <= 0 || value >= 1000000) throw new ArgumentOutOfRangeException();
+                //_salary = value;
+                if (0 < value && value < 1000000) _salary = value;
+                else throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name + " " + Salary;
+        }
+    }
+}
---
+++ TeacherLibTests/TeacherTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TeacherLib.Tests
+{
+    [TestClass]
+    public class TeacherTests
+    {
+        [TestMethod]
+        public void TestSalary()
+        {
+            Teacher teacher = new Teacher("Anders", 1);
+            //teacher.Salary = 1;
+            Assert.AreEqual(1, teacher.Salary);
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => teacher.Salary = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => teacher.Salary = 1000000);
+        }
+
+        [TestMethod]
+        public void TestName()
+        {
+            Teacher teacher = new Teacher("Ib", 100);
+            //teacher.Name = "Ib";
+            Assert.AreEqual("Ib", teacher.Name);
+
+            Assert.ThrowsException<ArgumentException>(() => teacher.Name = "Y");
+        }
+
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Teacher teacher = new Teacher("Anders", 100);
+            Assert.AreEqual("Anders 100", teacher.ToString());
+        }
+
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Assert.ThrowsException<ArgumentException>(() => new Teacher("A", 100));
+        }
+
+        [TestMethod]
+        public void TestPi()
+        {
+            Assert.AreEqual(3.1415, Teacher.Pi, 0.0001);
+        }
+    }
+}
